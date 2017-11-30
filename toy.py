cb66c2bcbb65e945ef2ab6671fd07feaf18cc0de
David J. Harris <davharris@users.noreply.github.com>
2017-11-30 13:07:23 -0500

    Better initialization, etc

--- toy.py
+++ toy.py
@@ -24,19 +24,19 @@ print(tf.__version__)
 
 # In[2]:
 
-N = 1000 # Number of data points
+N = 500 # Number of data points
 
 # One uniformly-distributed predictor, one Gaussian-distributed predictor
 x_train = np.column_stack([np.linspace(-2, 2, num=N), np.random.normal(0, 1, N)]).astype(np.float32)
 
 # Only the first predictor matters
-w = np.array([[3, -3], [0,0]])
+w = np.array([[3, -3, 0], [0,0,0]])
 
 # "True" latent variable" for each data point
 true_z = np.random.normal(0, .5, N)
 
 # Expected abundance
-mu = np.exp(3 + np.sin(np.dot(x_train, w)) + np.column_stack([true_z, true_z]))
+mu = np.exp(3 + np.sin(np.dot(x_train, w)) + np.column_stack([true_z, true_z, true_z]))
 
 # Observed abundances
 y_train = np.random.poisson(mu).astype(np.int32)
@@ -45,15 +45,17 @@ y_train = np.random.poisson(mu).astype(np.int32)
 # In[3]:
 
 # Mu vs x
-plt.scatter(x_train[:,0], mu[:,0], s=15, edgecolors='none');
-plt.scatter(x_train[:,0], mu[:,1], s=15, edgecolors='none');
+plt.scatter(x_train[:,0], mu[:,0], s=15, alpha = 0.25);
+plt.scatter(x_train[:,0], mu[:,1], s=15, alpha = 0.25);
+plt.scatter(x_train[:,0], mu[:,2], s=15, alpha = 0.25);
 
 
 # In[4]:
 
 # Mu vs true_z
-plt.scatter(true_z, mu[:,0], s=15, edgecolors='none');
-plt.scatter(true_z, mu[:,1], s=15, edgecolors='none');
+plt.scatter(true_z, mu[:,0], s=15, alpha = 0.25);
+plt.scatter(true_z, mu[:,1], s=15, alpha = 0.25);
+plt.scatter(true_z, mu[:,2], s=15, alpha = 0.25);
 
 
 # # Model
@@ -74,7 +76,7 @@ z = Normal(loc=tf.zeros([N, n_z]), scale=tf.ones([N,n_z]))
 
 # Running this twice will throw an error because it can't overwrite the variables in
 # the layers' scope
-# Note that the regularizers in tf.layers have no effect on Edward models
+# Note that the regularizers in tf.layers have no effect on Edward models!
 with tf.variable_scope("network"):
     # Concatenate the inputs to the neural net
     xz = tf.concat([x_train, z], 1)
@@ -86,8 +88,12 @@ with tf.variable_scope("network"):
     # Low-dimensional "environment", to which all species respond in a generalized linear way.
     env = tf.layers.dense(h, 10, activation=tf.nn.elu, name="env")
 
-    # Expected abundances
-    yhat = tf.layers.dense(env, 2, activation=tf.exp, name="out")
+    # Expected abundances. Initialize the biases so they tend to produce values with the right geometric mean
+    yhat = tf.layers.dense(env, 
+                           y_train.shape[1], 
+                           activation=tf.exp, 
+                           name="out", 
+                           bias_initializer=tf.constant_initializer(np.log(np.mean(y_train, 0)).astype(np.float32)))
 
 
 # ### Outputs
@@ -126,7 +132,8 @@ inference = ed.KLqp({z: qz}, data={y: y_train})
 inference.initialize(var_list=tf.trainable_variables(), 
                      n_samples=10,
                      global_step = global_step,
-                     kl_scaling={z: 1})
+                     kl_scaling={z: 1},
+                     optimizer=tf.train.AdamOptimizer(learning_rate=.001))
 
 # Start a session, then initialize all the variables
 sess = ed.get_session()
@@ -135,51 +142,55 @@ tf.global_variables_initializer().run()
 
 # ### Run
 
-# In[10]:
+# In[20]:
 
-for _ in range(25000):
+for _ in range(10000):
     inference.update()
 
 
 # # Diagnostics
 
-# In[11]:
+# In[21]:
 
 print(global_step.eval())
 print(inference.loss.eval())
 
 
-# In[12]:
+# In[22]:
 
+which_species = 1
+
+# Predictions using the prior on z
 plt.figure(figsize=(10, 10))
-plt.scatter(x_train[:,0], np.log(mu[:,0]));
+plt.scatter(x_train[:,0], np.log(mu[:,which_species]));
 for _ in range(20):
-    plt.scatter(x_train[:,0], np.log(yhat.eval()[:,0]), alpha=0.1, c="black", s=10);
+    plt.scatter(x_train[:,0], np.log(yhat.eval()[:,which_species]), alpha=0.1, c="black", s=10);
 
 
-# In[13]:
+# In[23]:
 
+# Predictions using the approximate posterior on z
 plt.figure(figsize=(10, 10))
-plt.scatter(x_train[:,0], np.log(mu[:,0]));
+plt.scatter(x_train[:,0], np.log(mu[:,which_species]));
 for _ in range(20):
-    plt.scatter(x_train[:,0], np.log(qyhat.eval()[:,0]), alpha=0.1, c="darkred", s=10);
+    plt.scatter(x_train[:,0], np.log(qyhat.eval()[:,which_species]), alpha=0.1, c="darkred", s=10);
 
 
-# In[14]:
+# In[24]:
 
 plt.hist(qz.stddev().eval(), bins=50);
 
 
-# In[26]:
+# In[25]:
 
-# Approximate posterior distributions of z, given x. Should be no big trends or gaps
-for _ in range(n_z):
+# Approximate posterior distributions of z, given x. Should be no big (low-frequency?) trends or gaps
+for i in range(n_z):
     for _ in range(25):
-        plt.scatter(x_train[:,0], qz.eval()[:,which_latent], alpha=0.1, c="darkred", s=5)
+        plt.scatter(x_train[:,0], qz.eval()[:,i], alpha=0.1, c="darkred", s=5)
     plt.show()
 
 
-# In[27]:
+# In[26]:
 
 nn = 1000
 xx = 0
@@ -197,23 +208,24 @@ plt.show()
 plt.scatter(np.log(qxx[:,0].flatten()), np.log(mu[:,0]).flatten(), c="darkred");
 
 
+# In[27]:
+
+# "Residual correlation" among species
+np.corrcoef(np.log(yhat.eval() / xx).T)
+
+
 # # Other outputs
 
-# In[24]:
+# In[18]:
 
 # List of parameters trained by the network.
 tf.get_collection(tf.GraphKeys.TRAINABLE_VARIABLES, scope="network")
 
 
-# In[18]:
+# In[19]:
 
 # Get the weights of the final layer (i.e. species-level coefficients)
 tf.get_default_graph().get_tensor_by_name('network/out/kernel:0').eval()
-
-
-# In[ ]:
-
-
 
 
 # In[ ]:
